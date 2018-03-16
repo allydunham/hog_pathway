2c4fac54ab18e013b085a9b12f1adf25d79869b9
allydunham <ally@ebi.ac.uk>
2018-03-16 11:20:25 +0000

    Tidied plots and added plots for Sorbitol 1mM and NaCl 0.6mM

--- bin/genetic_distance.R
+++ bin/genetic_distance.R
@@ -15,55 +15,19 @@ rownames(growth) <- strain_to_sys[rownames(growth)]
 
 genotypes <- genotypes[rownames(growth),]
 
-genetic_distance <- as.matrix(dist(genotypes, method = "manhattan"))
-growth_distance <- as.matrix(dist(growth[,1]))
+genetic_distance <- as.matrix(dist(genotypes, method = "man"))
+growth_distance_NaCl4 <- as.matrix(dist(growth[,1]))
+growth_distance_NaCl6 <- as.matrix(dist(growth[,2]))
+growth_distance_Sorb <- as.matrix(dist(growth[,3]))
 
-plot(genetic_distance[upper.tri(genetic_distance)], growth_distance[upper.tri(growth_distance)])
+plot(genetic_distance[upper.tri(genetic_distance)], growth_distance_NaCl4[upper.tri(growth_distance_NaCl4)],
+     pch=20, xlab="Genetic Distance (Manhatten)", ylab="Difference in S-Score (NaCl 0.4mM)")
 
+plot(genetic_distance[upper.tri(genetic_distance)], growth_distance_NaCl6[upper.tri(growth_distance_NaCl6)],
+     pch=20, xlab="Genetic Distance (Manhatten)", ylab="Difference in S-Score (NaCl 0.6mM)")
 
-## Genotype against growth
-impact <- read.table('data/hog-gene-variants.impact', sep='\t', header=TRUE)
-impact <- impact[impact$mut_id %in% colnames(genotypes),]
-
-# Only consider non-synonymous coding changes (need to reduce number of variables to consider)
-impact <- impact[!impact$type == "synonymous",]
-impact <- impact[!is.na(impact$id),]
-
-data("BLOSUM62")
-impact$blosum <- apply(impact, 1, function(x){
-  if (x["alt_aa"] %in% rownames(BLOSUM62)){
-    return(BLOSUM62[unlist(x["ref_aa"]),unlist(x["alt_aa"])])
-  } else {
-    return(NA)
-  }
-  })
-
-## Only consider subs that generally occur less than expected
-impact <- impact[is.na(impact$blosum) | impact$blosum < 0,]
-
-genotypes <- genotypes[,unique(impact$mut_id)]
-genotypes <- genotypes[,colSums(genotypes) < dim(genotypes)[1]*2*0.1]
-
-gen_df <- as.data.frame(genotypes)
-gen_df$growth <- as.numeric(growth$sodium.chloride.0.6mM)
-
-library(caret)
-train_index <- createDataPartition(gen_df$growth, times = 1, p = 0.8, list = FALSE)
-training <- gen_df[train_index,]
-test <- gen_df[-train_index,]
-  
-model <- train(growth ~ ., data = training, method = "rf")
-
-model <- train(growth ~ ., data = training, method = "bayesglm")
-
-
-prediction <- predict(model, newdata = test)
-plot(test$growth, prediction)
-
-
-
-
-
+plot(genetic_distance[upper.tri(genetic_distance)], growth_distance_Sorb[upper.tri(growth_distance_Sorb)],
+     pch=20, xlab="Genetic Distance (Manhatten)", ylab="Difference in S-Score (Sorbitol 1mM)")
 
 
 
