2165b5d531daede6976df94c6f858a1b6a97e0b8
allydunham <ally@ebi.ac.uk>
2018-03-26 17:05:24 +0100

    Fixed __repr__ and initialisation from this representation

--- bin/binary_pathway.py
+++ bin/binary_pathway.py
@@ -12,17 +12,17 @@ FUNCTIONALITY_THRESHOLD = 0.5
 
 class ProteinNetwork:
     """Signalling network"""
-    def __init__(self, inputs=(), outputs=()): #, proteins=None
+    def __init__(self, proteins=None, inputs=(), outputs=()):
         self.proteins = {}
-        # if not proteins is None:
-        #     for name, data in proteins.items():
-        #         self.node(name, **data)
+        if not proteins is None:
+            for protein in proteins.values():
+                self.node(protein)
 
         self.inputs = set(inputs)
         self.outputs = set(outputs)
 
     def __repr__(self):
-        return "ProteinNetwork(proteins={}, inputs={}, outputs={}, threshold={})\
+        return "ProteinNetwork(proteins={}, inputs={}, outputs={})\
                 ".format(self.proteins, self.inputs, self.outputs)
 
     def __str__(self):
@@ -100,10 +100,12 @@ class Protein:
         self.function = function
 
     def __repr__(self):
-        return self.name
+        return "Protein('{}', inputs={}, outputs={}, function={})".format(
+            self.name, self.inputs, self.outputs, self.function
+            )
 
     def __str__(self):
-        return self.name
+        return self.__repr__()
 
     def add_input(self, name):
         """Add an input edge"""
@@ -121,8 +123,12 @@ class ProteinComplex(Protein):
     """Protein network component made up of multiple individual components"""
     def __init__(self, name, components, inputs=None, outputs=None):
         Protein.__init__(self, name, inputs, outputs, None)
-        self.components = {name: Protein(name, inputs=None, outputs=None, function=func)
-                           for name, func in components.items()}
+        self.components = {x.name: x for x in components}
+
+    def __repr__(self):
+        return "ProteinComplex('{}', {},inputs={}, outputs={})".format(
+            self.name, [x for x in self.components.values()], self.inputs, self.outputs
+            )
 
     def get_activity(self):
         """Test if the complex is active"""
@@ -130,18 +136,24 @@ class ProteinComplex(Protein):
 
 
 if __name__ == "__main__":
-    ## Can implement reduced version of hog with only direct interactions
-    ## (i.e ignoring scaffold proteins and negative regulators)
+    ## Can implement reduced version of hog without negative regulators (e.g. phosphatases)
     hog = ProteinNetwork()
     hog.node(Protein('cdc42'))
-    hog.node(ProteinComplex('sho1-hkr1-msb1', {'sho1': 1, 'hkr1': 1, 'msb1': 1}))
-    hog.node(Protein('sln1'))
+    hog.node(ProteinComplex('sho1-hkr1-msb1',
+                            [Protein('sho1'), Protein('hkr1'), Protein('msb1')]))
 
-    hog.node(ProteinComplex('cla4-ste20', {'cla4':1, 'ste20': 1}, inputs=['cdc42']))
+    hog.node(Protein('sln1'))
+    hog.node(ProteinComplex('cla4-ste20',
+                            [Protein('cla4'), Protein('ste20')],
+                            inputs=['cdc42']))
+
     hog.node(Protein('ste11', inputs=['cla4-ste20', 'sho1-hkr1-msb1']))
     hog.node(Protein('ypd1', inputs=['sln1']))
     hog.node(Protein('ssk1', inputs=['ypd1']))
-    hog.node(ProteinComplex('ssk2-ssk22', {'ssk2': 1, 'ssk22': 1}, inputs=['ssk1']))
+    hog.node(ProteinComplex('ssk2-ssk22',
+                            [Protein('ssk2'), Protein('ssk22')],
+                            inputs=['ssk1']))
+
     hog.node(Protein('pbs2', inputs=['ste11', 'ssk2-ssk22']))
     hog.node(Protein('hog1', inputs=['pbs2']))
     hog.node(Protein('hot1', inputs=['hog1']))
