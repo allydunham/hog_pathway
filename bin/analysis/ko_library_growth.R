47345abb5269e9035d4b705eef19a89a80bee6da
allydunham <ally@ebi.ac.uk>
2018-07-23 17:32:14 +0100

    Added comparison dendograms and sorted erroneous correlation

--- bin/analysis/ko_library_growth.R
+++ bin/analysis/ko_library_growth.R
@@ -7,6 +7,7 @@ library(ggpubr)
 library(gplots)
 library(ggdendro)
 library(dendextend)
+library(plotly)
 
 #### Import and Process Data ####
 genes <- readRDS('data/Rdata/gene_meta_all.rds')
@@ -17,21 +18,24 @@ non_essential_genes <- filter(essential, essential == 'NE') %>% pull(locus)
 ko_growth <- read_tsv('data/raw/ko_scores.txt', col_names = TRUE) %>%
   filter(!gene == 'WT') %>%
   filter(!duplicated(.[,c('strain', 'condition', 'gene')])) %>%
-  select(-position)
+  select(-position) %>%
+  mutate(condition = gsub('  ', ' ', condition)) # Some conditions have double spaces in names
+
+strain_combs <- combn(c('S288C', 'UWOP', 'Y55', 'YPS'), 2)
 
 # Does not apply sorting because ko_scores tsv comes sorted
-split_strains <- function(str, var){
+split_strains <- function(str, var, tbl){
   # Function to filter ko_growth to a given strain with a matrix of condition against 'var' (score or qvalue)
-  ko <- filter(ko_growth, strain == str) %>%
+  ko <- filter(tbl, strain == str) %>%
     select(condition, gene, !!var) %>%
     spread(key = gene, value = !!var)
   return(ko)
 }
 
-strain_ko_scores <- lapply(unique(ko_growth$strain), split_strains, var='score')
+strain_ko_scores <- lapply(unique(ko_growth$strain), split_strains, var='score', tbl=ko_growth)
 names(strain_ko_scores) <- unique(ko_growth$strain)
 
-strain_ko_qvalues <- lapply(unique(ko_growth$strain), split_strains, var='qvalue')
+strain_ko_qvalues <- lapply(unique(ko_growth$strain), split_strains, var='qvalue', tbl=ko_growth)
 names(strain_ko_qvalues) <- unique(ko_growth$strain)
 
 #### Analysis ####
@@ -48,29 +52,55 @@ p_dend_list <- lapply(strain_con_dends, ggdendrogram, rotate=TRUE)
 p_dends <- ggarrange(plotlist = p_dend_list, ncol = 2, nrow = 2, labels = names(strain_con_dends))
 ggsave('figures/ko_growth/strain_condition_dendograms.pdf', p_dends, width = 10, height = 10)
 
+# dendogram comparisons
+# must filter Maltose 2%  (72H) since no data is available for Y55
+strain_ko_scores_filtered <- lapply(unique(ko_growth$strain), split_strains, var='score', tbl=filter(ko_growth, !condition == 'Maltose 2% (72H)'))
+names(strain_ko_scores_filtered) <- unique(ko_growth$strain)
+strain_con_dends_filtered <- lapply(strain_ko_scores_filtered, get_dend)
+
+pdf('figures/ko_growth/strain_condition_comparison_dendograms.pdf', width = 15, height = 20)
+layout(matrix(1:(6*3), nrow = 3, byrow = TRUE), widths = rep(c(2,0.75,2), 2))
+for (c in 1:dim(strain_combs)[2]){
+  strains <- strain_combs[,c]
+  tanglegram(untangle(strain_con_dends_filtered[[strains[1]]], strain_con_dends_filtered[[strains[2]]]),
+             margin_inner = 15, common_subtrees_color_branches = TRUE,
+             main_left = strains[1], main_right = strains[2], lwd=2, edge.lwd=2, just_one = FALSE)
+}
+layout(matrix(1, nrow = 1))
+dev.off()
+
+
 ## Correlations
-# 
-get_cor <- function(x){
+get_cor <- function(x, cor_meth = 'pearson', cor_use = 'pairwise', upper_tri=TRUE){
   mat <- as.matrix(select(x, -condition))
   rownames(mat) <- x$condition
-  cors <- cor(t(mat), use = 'complete')
-  cors[lower.tri(cors, diag = TRUE)] <- NA
+  cors <- cor(t(mat), method = cor_meth, use = cor_use)
+  if (upper_tri){
+    cors[lower.tri(cors, diag = TRUE)] <- NA
+  }
   cors %<>% as_tibble(rownames='condition1') %>%
     gather(key = 'condition2', value = 'cor', -condition1) %>%
-    filter(!is.na(cor))
+    drop_na()
   return(cors)
 }
 
 strain_con_cors <- bind_rows(lapply(strain_ko_scores, get_cor), .id = 'strain') %>%
   arrange(strain, condition1, condition2) %>%
-  spread(strain, cor)
+  spread(strain, cor) %>%
+  unite(col = 'pair', condition1, condition2, remove = FALSE)
 
-combs <- combn(c('S288C', 'UWOP', 'Y55', 'YPS'), 2)
-p_strain_con_cor_cors <- mapply(function(x, y){ggplot(strain_con_cors, aes_string(x=x, y=y)) + geom_point()}, combs[1,], combs[2,], SIMPLIFY = FALSE)
-p_strain_con_cor_cors_arr <- ggarrange(plotlist = p_strain_con_cor_cors, ncol = 3, nrow = 2)
+p_strain_con_cor_cors <- mapply(function(x, y){
+  ggplot(strain_con_cors, aes_string(x=x, y=y, text='pair')) + 
+    geom_point(size=0.5) + 
+    geom_abline(intercept = 0, slope = 1, colour='firebrick2') +
+    geom_abline(intercept = -0.2, slope = 1, colour='firebrick2', linetype=2) +
+    geom_abline(intercept = 0.2, slope = 1, colour='firebrick2', linetype=2)
+  }, 
+  strain_combs[1,], strain_combs[2,], SIMPLIFY = FALSE)
+names(p_strain_con_cor_cors) <- paste(strain_combs[1,], strain_combs[2,], sep='_')
+
+# removed rows correspond to pairs including Maltose 2%  (72H), since no data is available for Y55
+p_strain_con_cor_cors_arr <- ggarrange(plotlist = p_strain_con_cor_cors, ncol = 3, nrow = 2, common.legend = TRUE)
 ggsave('figures/ko_growth/strain_condition_condition_cors.pdf', p_strain_con_cor_cors_arr, width = 7, height = 5)
 
-
-
-
-
+ggplotly(p_strain_con_cor_cors$Y55_YPS, tooltip = c('pair'))
