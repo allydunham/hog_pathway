be507775cda41204d4a81ec93b25f815deea8b08
allydunham <ally@ebi.ac.uk>
2021-05-27 21:31:31 +0100

    Added more panels

--- bin/thesis_figure.R
+++ bin/thesis_figure.R
@@ -3,23 +3,8 @@
 library(tidyverse)
 library(ggpubr)
 library(multipanelfigure)
-library(Biostrings)
-data("BLOSUM62")
-blosum <- as_tibble(BLOSUM62, rownames = "wt") %>%
-  pivot_longer(-wt, names_to = "mut", values_to = "blosum62")
-  
-blank_plot <- function(text = NULL){
-  p <- ggplot(tibble(x=c(0, 1)), aes(x=x, y=x)) +
-    geom_blank() +
-    theme(panel.grid.major.y = element_blank(),
-          axis.ticks = element_blank(),
-          axis.text = element_blank(),
-          axis.title = element_blank())
-  
-  if (!is.null(text)){
-    p <- p + annotate(geom = 'text', x = 0.5, y = 0.5, label = text)
-  }
-}
+library(ggtext)
+library(png)
 
 theme_set(theme_pubclean() + theme(legend.position = 'right',
                                    plot.title = element_text(hjust = 0.5),
@@ -27,7 +12,64 @@ theme_set(theme_pubclean() + theme(legend.position = 'right',
                                    strip.background = element_blank(),
                                    legend.key = element_blank()))
 
-### Panel  - P(Aff) ###
+# Make pretty p value categories for plots
+pretty_p_values <- function(p, breaks = c(0.001, 0.01, 0.05), markdown_exp=FALSE, prefix_p=FALSE){
+  breaks <- sort(breaks, decreasing = TRUE)
+  break_str <- as.character(breaks)
+  gt <- '>'
+  lt <- '<'
+  
+  if (markdown_exp){
+    break_str <- str_replace(break_str, "^1?e(-?[0-9]*\\.?[0-9]*)", "10<sup>\\1</sup>")
+    gt <- '&gt;'
+    lt <- '&lt;'
+  }
+  
+  p_out <- rep(str_c(gt, ' ', break_str[1]), length(p))
+  for (i in 1:length(breaks)){
+    p_out[p < breaks[i]] <- str_c(lt, ' ', break_str[i])
+  }
+  break_levels <- c(str_c(gt, ' ', break_str[1]), unlist(map(break_str, ~str_c(lt, ' ', .))))
+  
+  if (prefix_p){
+    p_out <- str_c('p ', p_out)
+    break_levels <- str_c('p ', break_levels)
+  }
+  
+  return(factor(p_out, levels = break_levels))
+}
+
+#### Load Data ####
+data("BLOSUM62", package = "Biostrings")
+blosum <- as_tibble(BLOSUM62, rownames = "wt") %>%
+  pivot_longer(-wt, names_to = "mut", values_to = "blosum62")
+
+strains <- readRDS('data/Rdata/strain_meta.rds')
+filtered_strains <- filter(strains, Ploidy <= 2, Aneuploidies == 'euploid') %>%
+  pull(`Standardized name`) %>%
+  setdiff(. , c("AMH", "BAG", "BAH", "BAL", "CEG", "CEI"))
+
+genes <- readRDS('data/Rdata/gene_meta_all.rds')
+essential <- readRDS('data/Rdata/essential_genes.rds')
+
+growth <- read_tsv('data/raw/yeasts_liti_fixed.tsv', col_names = TRUE, col_types = cols(strain = col_character())) %>%
+  filter(info %in% filtered_strains) %>% 
+  filter(subset == 'liti') %>%
+  rename(strain_id = strain, strain=info)
+
+ko_thresh <- 0.5
+probs <- readRDS('data/Rdata/paff_all_genes.rds') %>%
+  filter(strain %in% growth$strain) %>%
+  mutate(ko = p_aff > ko_thresh)
+
+associations <- read_tsv("data/gene_paff_growth_assocations.tsv") %>%
+  left_join(select(genes, gene = id, name), by = "gene") %>%
+  left_join(select(essential, gene = locus, essential), by = "gene")
+
+path <- readRDS('data/Rdata/hog_path_probs.rds') %>%
+  filter(strain %in% unique(growth$strain))
+
+#### Panel - P(Aff) ####
 jelier <- read_tsv("data/jelier.impact") %>%
   select(gene, position = pos_aa, wt = ref_aa, mut = alt_aa, effect, foldx = foldx_ddG, sift = sift_score) %>%
   left_join(blosum, by = c("wt", "mut")) %>%
@@ -63,12 +105,51 @@ p_paff <- ggplot(mapping = aes(x = x, y = y, colour = model)) +
   labs(x = "", y = "P(Neut)") +
   theme(strip.placement = "outside")
 
+# BLOSUM Cor
+# cor.test(jelier$blosum62, f_sift(jelier$sift, coef(fit_sift)[1], coef(fit_sift)[2]))
+# cor.test(jelier$blosum62, f_foldx(jelier$foldx, coef(fit_foldx)[1], coef(fit_foldx)[2]))
 
-### Panel  - Gene Phenotype Associations ###
-# Summarise associations between gene paff and phenoypes
-p_associations <- blank_plot("P(Aff) Associations")
+#### Panel - Gene Phenotype Association Summary ####
+association_summary <- group_by(associations, condition) %>%
+  summarise(Essential = sum(p_adj < 0.01 & essential == "E", na.rm = TRUE),
+            `Non-Essential` = sum(p_adj < 0.01 & essential == "NE", na.rm = TRUE)) %>%
+  pivot_longer(-condition, names_to = "type", values_to = "count") %>%
+  drop_na()
+
+p_associations <- ggplot(association_summary, aes(x = reorder(condition, count), y = count, fill = type)) +
+  geom_col(width = 0.75) +
+  coord_flip() +
+  scale_fill_manual(name = "", values = c(Essential = "#4daf4a", `Non-Essential` = "#377eb8")) +
+  scale_y_continuous(expand = expansion(0)) +
+  labs(y = "Significant Genes (P<sub>adj</sub> < 0.01)", x = "") +
+  theme(panel.grid.major.x = element_line(linetype = "dotted", colour = "grey"),
+        panel.grid.major.y = element_blank(),
+        axis.title.x = element_markdown(),
+        axis.ticks.y = element_blank(),
+        legend.position = "top")
+
+#### Panel - Example Gene Phenotype Associations ####
+association_examples <- left_join(probs, select(genes, gene = id, name), by = "gene") %>%
+  filter(name %in% c("HOG1", "PBS2", "SER3", "CDC42")) %>%
+  left_join(filter(select(growth, condition, strain, score), condition == "NaCl 0.6M (72H)"), by = "strain") %>%
+  mutate(ko_desc = ifelse(ko, "P(Aff) > 0.5", "P(Aff) ≤ 0.5"),
+         name = factor(name, levels = c("HOG1", "PBS2", "CDC42", "SER3")))
+
+p_association_examples <- ggplot(association_examples, aes(x = ko_desc, y = score, fill = ko_desc)) +
+  facet_wrap(~name, nrow = 1) +
+  geom_boxplot(outlier.shape = 20) +
+  stat_compare_means(method = "wilcox", comparisons = list(c("P(Aff) > 0.5", "P(Aff) ≤ 0.5"))) +
+  stat_summary(geom = "text", fun.data = function(x) {data.frame(y = -4.4, label = length(x))}) +
+  scale_fill_manual(name = "", values = c(`P(Aff) > 0.5` = "firebrick2", `P(Aff) ≤ 0.5` = "cornflowerblue")) +
+  labs(x = "", y = "S-Score") +
+  theme(axis.ticks.x = element_blank(),
+        axis.text.x = element_blank(),
+        legend.position = "bottom",
+        legend.box.margin = margin(-20, 0, 0, 0),
+        legend.margin = margin(0, 0, 0, 0))
 
 ### Panel - HOG Pathway ###
+path_elements <- tibble(x = 2, y = 2, colour = c("Adaptor", "GTPase", "Kinase", "Phosphatase", "Sensor", "TF"))
 p_hog <- ggplot(path_elements, aes(x = x, y = y, colour = colour)) +
   geom_point(shape = 15) +
   lims(x = c(0, 0.8), y = c(0, 1)) +
@@ -83,23 +164,48 @@ p_hog <- ggplot(path_elements, aes(x = x, y = y, colour = colour)) +
         legend.margin = margin(0,0,0,0),
         legend.box.margin = margin(-10, 0,-10,-40))
 
-### Panel - Pathway Models ###
+#### Panel - HOG Pathway Models ####
 # Summarise performance of different models - plot Rsquared, cor and prob?
-p_pathway <- blank_plot("Pathway Models")
+gene_counts <- filter(associations, p_adj < 0.01, mean_ko < mean_active, essential == "NE") %>%
+  select(condition, gene) %>%
+  left_join(probs, by = "gene") %>%
+  group_by(strain, condition) %>%
+  summarise(sig_ko_count = sum(ko), .groups = "drop")
 
-### Figure Assembly ###
-size <- theme(text = element_text(size = 8))
+path_models <- left_join(gene_counts, select(path, strain, hog_probability), by = "strain") %>%
+  left_join(select(growth, condition, sscore = score, strain), by = c("strain", "condition")) %>%
+  pivot_longer(c(-strain, -sscore, -condition), names_to = "model", values_to = "value") %>%
+  group_by(model, condition) %>%
+  group_modify(~broom::glance(lm(sscore ~ value, data = .))) %>%
+  ungroup() %>%
+  mutate(model = c(hog_probability = "P(HOG Pathway)", sig_ko_count = "Sig Gene Count")[model],
+         log_p = log10(p.value),
+         p_cat = pretty_p_values(p.value, breaks = c(1e-10, 1e-5, 0.0001, 0.001, 0.01, 0.05, 0.1)))
+
+condition_levels <- filter(path_models, model == "P(HOG Pathway)") %>% arrange(r.squared) %>% pull(condition)
+
+p_pathway <- ggplot(path_models, aes(x = factor(condition, levels = condition_levels), y = r.squared, fill = p_cat)) +
+  facet_wrap(~model, nrow = 1) +
+  geom_col() +
+  coord_flip() +
+  scale_fill_brewer(palette = "YlGnBu") +
+  labs(x = "", y = expression(r^2))
+
+#### Figure Assembly ####
+size <- theme(text = element_text(size = 10))
 p1 <- p_paff + labs(tag = 'A') + size
 p2 <- p_associations + labs(tag = 'B') + size
-p3 <- p_hog + labs(tag = 'C') + size
-p4 <- p_pathway + labs(tag = 'D') + size
+p3 <- p_association_examples + labs(tag = 'C') + size
+p4 <- p_hog + labs(tag = 'D') + size
+p5 <- p_pathway + labs(tag = 'E') + size
 
 figure <- multi_panel_figure(width = 360, height = 240, columns = 3, rows = 2,
                               panel_label_type = 'none', row_spacing = 0, column_spacing = 0) %>%
   fill_panel(p1, row = 1, column = 1) %>%
-  fill_panel(p2, row = 1, column = 2:3) %>%
-  fill_panel(p3, row = 2, column = 1) %>%
-  fill_panel(p4, row = 2, column = 2:3)
+  fill_panel(p2, row = 1, column = 2) %>%
+  fill_panel(p3, row = 1, column = 3) %>%
+  fill_panel(p4, row = 2, column = 1) %>%
+  fill_panel(p5, row = 2, column = 2:3)
 
 ggsave('figures/thesis_figure.pdf', figure, width = figure_width(figure), height = figure_height(figure), units = 'mm')
 ggsave('figures/thesis_figure.tiff', figure, width = figure_width(figure), height = figure_height(figure), units = 'mm')
