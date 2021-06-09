a1e5606c53c24c91fbca4353b682589e2e099c82
allydunham <ally@ebi.ac.uk>
2021-06-09 17:51:52 +0100

    Refined calculation and switch to Holm P value adjustment

--- bin/paff_gene_asscociations.R
+++ bin/paff_gene_asscociations.R
@@ -15,29 +15,22 @@ growth <- read_tsv('data/raw/yeasts_liti_fixed.tsv', col_names = TRUE, col_types
   filter(subset == 'liti') %>%
   rename(strain_id = strain) %>%
   rename(strain = info) %>%
-  select(strain, condition, score)
+  select(strain, condition, score, qvalue)
 
 ko_thresh <- 0.5
 probs <- readRDS('data/Rdata/paff_all_genes.rds') %>%
   filter(strain %in% growth$strain) %>%
   mutate(ko = p_aff > ko_thresh)
 
-test_gene <- function(tbl) {
-  if (sum(tbl$ko) == 0) {
-    return(tibble(condition=NA, n=nrow(tbl), n_ko=0, p_value=NA, mean_active = NA, mean_ko = NA))
-  }
-  left_join(tbl, growth, by = "strain") %>%
-    group_by(condition) %>%
-    summarise(p_value = ifelse(sum(ko) > 0, wilcox.test(score[ko], score[!ko])$p.value, NA),
-              mean_active = mean(score[!ko], na.rm = TRUE),
-              mean_ko = mean(score[ko], na.rm = TRUE),
-              .groups = "drop") %>%
-    mutate(n = nrow(tbl), n_ko = sum(tbl$ko)) %>%
-    select(condition, n, n_ko, p_value, mean_active, mean_ko)
-}
-
-associations <- group_by(probs, gene) %>%
-  group_modify(~test_gene(.x)) %>%
+associations <- left_join(probs, growth, by = "strain") %>%
+  group_by(gene, condition) %>%
+  filter(!sum(ko) == 0) %>%
+  summarise(p_value = wilcox.test(score[ko], score[!ko])$p.value,
+            mean_active = mean(score[!ko], na.rm = TRUE),
+            mean_ko = mean(score[ko], na.rm = TRUE),
+            n = n(),
+            n_ko = sum(ko),
+            .groups = "drop") %>%
   mutate(n_active = n - n_ko,
-         p_adj = p.adjust(p_value, method = "fdr"))
+         p_adj = p.adjust(p_value, method = "holm"))
 write_tsv(associations, "data/gene_paff_growth_assocations.tsv")
